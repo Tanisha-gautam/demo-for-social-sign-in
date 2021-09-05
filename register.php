8a09281cf66741df1a9ba44152df350425c85e1e
Tanisha gautam <55926866+Tanisha-gautam@users.noreply.github.com>
2021-09-06 01:44:02 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,49 +1,55 @@
-<?php include('login.php') ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Social auth sign in</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-</head>
-<body>
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">DEMO</label>
-        <ul>
-            <li><a class="active" href="#">HOME</a></li>
-            <li><a href="#">INITIATIVES</a></li>
-            <li><a href="#">EVENTS</a></li>
-            <li><a href="#">TEAM</a></li>
-            <li><a href="#">BLOG</a></li>
-            <li><a href="#">CONTACT</a></li>
-        </ul>
-    </nav>
-    <div class="login-box">
-    <div class="left-b0x">
-        <h1>Sign Up</h1>
-        <form method="POST" action="register.php">
-            <div calss="form_input">
-                <input type="text" class="input-box" placeholder="username"required>
-            </div>
-            <div calss="form_input">
-                <input type="email" class="input-box" placeholder="email"required>
-            </div>
-            <div class="form_input">
-                <input type="password" class="input-box" placeholder="enter password"required>
-            </div>
-            <div class="form_input">
-                <input type="password" class="input-box" placeholder="ee-enter p assword"required>
-            </div> 
-                <input type="submit" class="submit-btn" value="Sign Up">
-        </form>
-    </div>
-         <p>Already a user<a href="sign.php"><br><b>log in</b></a></p>
-    </div>
-    
-</body>
-</html>
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) && isset($_POST['email']) &&
+        isset($_POST['password']) && isset($_POST['repassword'])){
+        
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $repassword = $_POST['repassword'];
+       
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "server";
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(username, email, password, repassword) values(?, ?, ?, ?)";
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssss",$username, $email, $password, $repassword);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
